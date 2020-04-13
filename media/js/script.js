4fd9882382dc9263840f8c6c9f6ccc643a56f2af
Artem Vasilev <kern.usr@gmail.com>
2020-04-13 23:33:38 +0300

    Переписал и оптимизировал js

--- media/js/script.js
+++ media/js/script.js
@@ -7,90 +7,133 @@
  * @link       https://webmasterskaya.xyz/
  */
 
-jQuery(function() {
-	dadataSuggestionsInit();
-});
+if (!Object.assign) {
+	Object.defineProperty(Object, 'assign', {
+		enumerable: false,
+		configurable: true,
+		writable: true,
+		value: function(target, firstSource) {
+			'use strict';
+			if (target === undefined || target === null) {
+				throw new TypeError('Cannot convert first argument to object');
+			}
 
-function dadataSuggestionsInit() {
-	var dadataParams = {
-		token: Joomla.getOptions('plg_jshopping_dadata_suggestions').token,
-		type: 'ADDRESS',
-		onSelect: function(suggestion) {
-			window.lastValue = suggestion.value;
-			if (this.name.substr(0, 2) == '_d') {
-				dataSetFromSuggestion(suggestion.data, 'd_');
-			}
-			else {
-				dataSetFromSuggestion(suggestion.data, '');
-			}
-		},
-		onSelectNothing: function(query) {
-			if (this.value != '') {
-				this.value = window.lastValue ? window.lastValue : '';
-			}
-			else {
-				if (this.name.substr(0, 2) == '_d') {
-					dataSetFromSuggestion({}, 'd_');
+			var to = Object(target);
+			for (var i = 1; i < arguments.length; i++) {
+				var nextSource = arguments[i];
+				if (nextSource === undefined || nextSource === null) {
+					continue;
 				}
-				else {
-					dataSetFromSuggestion({}, '');
+
+				var keysArray = Object.keys(Object(nextSource));
+				for (var nextIndex = 0, len = keysArray.length; nextIndex <
+				len; nextIndex++) {
+					var nextKey = keysArray[nextIndex];
+					var desc = Object.getOwnPropertyDescriptor(nextSource,
+						nextKey);
+					if (desc !== undefined && desc.enumerable) {
+						to[nextKey] = nextSource[nextKey];
+					}
 				}
 			}
+			return to;
 		},
-	};
-
-	if (!Joomla.getOptions('plg_jshopping_dadata_suggestions').checkout) {
-		return;
-	}
-	var dadataAddressField = jQuery(
-		'#' +
-		Joomla.getOptions('plg_jshopping_dadata_suggestions').checkout.element),
-		dadataDAddressField = jQuery(
-			'#d_' + Joomla.getOptions(
-			'plg_jshopping_dadata_suggestions').checkout.element);
-
-	dadataAddressField.suggestions(dadataParams);
-	dadataDAddressField.suggestions(dadataParams);
-
-	dadataAddressField.on('focus', function() {
-		window.lastValueD = this.value;
-	});
-	dadataDAddressField.on('focus', function() {
-		window.lastValueD = this.value;
 	});
 }
 
-function dataSetFromSuggestion(data, prefix) {
-	jQuery('[name="' + prefix + 'zip"]').
-		val(data['postal_code'] ? data['postal_code'] : '');
-	jQuery('[name="' + prefix + 'state"]').
-		val(data['region_with_type'] ? data['region_with_type'] : '');
-	if(!!data['city']){
-		jQuery('[name="' + prefix + 'city"]').
-			val(data['city'] ? data['city'] : '');
-	} else {
-		jQuery('[name="' + prefix + 'city"]').
-			val(data['settlement'] ? data['settlement'] : '');
+jQuery(function() {
+	if(typeof Joomla.getOptions('plg_jshopping_dadata_suggestions') !== 'undefined'){
+		var plgDaDataSuggestions = Joomla.getOptions('plg_jshopping_dadata_suggestions');
+		var helper = new suggestionHelper(plgDaDataSuggestions);
 	}
-	jQuery('[name="' + prefix + 'street"]').
-		val(data['street_with_type'] ? data['street_with_type'] : '');
-	var home = '';
-	if (data['house']) {
-		home += ' ' + data['house'];
+});
+
+function suggestionHelper(_params) {
+	var _helper = this,
+		_elements = {},
+		_suggestions = {
+			hint: false,
+			token: _params.token,
+			onSelectNothing: function(query) {
+				/*if (this.value != '') {
+					this.value = window.lastValue ? window.lastValue : '';
+				}
+				else {
+					_helper.setSelected(this.name, {});
+				}*/
+			},
+		};
+
+	var formatPostalCode = function(suggestion) {
+			return suggestion.data.postal_code || '';
+		}.bind(_helper),
+		formatRegion = function(suggestion) {
+			return suggestion.data.region_with_type || '';
+		}.bind(_helper),
+		formatCity = function(suggestion) {
+			var address = suggestion.data;
+			if (!!address.city_with_type) {
+				return address.city_with_type;
+			}
+			else {
+				return address.settlement_with_type;
+			}
+		}.bind(_helper),
+		formatStreet = function(suggestion) {
+			return suggestion.data.street_with_type || '';
+		}.bind(_helper),
+		formatHouse = function(suggestion) {
+			var address = suggestion.data,
+				house = [];
+			if (address.house) {
+				house.push(address.house);
+			}
+			if (address.block_type) {
+				house.push(address.block_type);
+			}
+			if (address.block) {
+				house.push(address.block);
+			}
+			return house.join(' ').trim() || '';
+		}.bind(_helper),
+		formatApartment = function(suggestion) {
+			var address = suggestion.data,
+				apartment = [];
+			if (address.flat_type) {
+				apartment.push(address.flat_type);
+			}
+			if (address.flat) {
+				apartment.push(address.flat);
+			}
+			return apartment.join(' ').trim() || '';
+		}.bind(_helper),
+		formatPatronymic = function(suggestion) {
+			return suggestion.data.patronymic || '';
+		}.bind(_helper);
+
+	for (var field in _params.fields) {
+		if (!_params.fields.hasOwnProperty(field)) {
+			continue;
+		}
+		_elements[field] = jQuery('[name="' + field + '"]');
+		if (_elements[field].type !== 'hidden') {
+
+			var options = Object.assign({}, _params.fields[field]);
+
+			if (!!_params.fields[field].formatSelected) {
+				options.formatSelected = eval(
+					_params.fields[field].formatSelected);
+			}
+
+			if (!!_params.fields[field].constraints &&
+				typeof _params.fields[field].constraints === 'string') {
+				options.constraints = _elements[_params.fields[field].constraints];
+			}
+
+			_elements[field].suggestions(
+				Object.assign({}, _suggestions, options)).bind(jQuery);
+		}
 	}
-	if (data['block_type']) {
-		home += ' ' + data['block_type'];
-	}
-	if (data['block']) {
-		home += ' ' + data['block'];
-	}
-	jQuery('[name="' + prefix + 'home"]').val(jQuery.trim(home));
-	var apartment = '';
-	if (data['flat_type']) {
-		apartment += ' ' + data['flat_type'];
-	}
-	if (data['flat']) {
-		apartment += ' ' + data['flat'];
-	}
-	jQuery('[name="' + prefix + 'apartment"]').val(jQuery.trim(apartment));
+
+	return _helper;
 }
